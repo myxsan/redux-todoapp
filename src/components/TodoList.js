60629025d2482a673a39feaeb803d2a7901afa97
myxsan <yksl3461@gmail.com>
2023-04-08 23:17:56 +0300

    toggle operations

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,11 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { toggle } from "../redux/todos/todosSlice";
 
 function Todolist() {
   const items = useSelector((state) => state.todos.items);
-
+    const dispatch = useDispatch();
+    
   return (
     <ul className="todo-list">
       {/* <li className="completed">
@@ -17,7 +19,7 @@ function Todolist() {
       {items.map((item) => (
         <li key={item.id} className={item.completed ? "completed" : ""}>
           <div className="view">
-            <input className="toggle" type="checkbox" />
+            <input className="toggle" type="checkbox" onChange={() => dispatch(toggle({id: item.id}))} />
             <label>{item.title}</label>
             <button className="destroy"></button>
           </div>
--- src/redux/todos/todosSlice.js
+++ src/redux/todos/todosSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { act } from "react-dom/test-utils";
 
 export const todosSlice = createSlice({
   name: "todos",
@@ -18,10 +19,15 @@ export const todosSlice = createSlice({
   },
   reducers: {
     addTodo: (state, action) => {
-        state.items.push(action.payload)
-    }
+      state.items.push(action.payload);
+    },
+    toggle: (state, action) => {
+      const { id } = action.payload;
+      const item = state.items.find((item) => item.id === id);
+      item.completed = !item.completed
+    },
   },
 });
 
-export const {addTodo} = todosSlice.actions
+export const { addTodo, toggle } = todosSlice.actions;
 export default todosSlice.reducer;
