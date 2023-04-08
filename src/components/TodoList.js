a31b207a28dd3a9aef4f4eeac018567b5743d386
myxsan <yksl3461@gmail.com>
2023-04-08 23:59:57 +0300

    destroy operations

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { toggle } from "../redux/todos/todosSlice";
+import { toggle, destroy } from "../redux/todos/todosSlice";
 
 function Todolist() {
   const items = useSelector((state) => state.todos.items);
-    const dispatch = useDispatch();
-    
+  const dispatch = useDispatch();
+  const handleDestroy = (id) => {
+    if (window.confirm("Are you sure?")) {
+      dispatch(destroy(id));
+    }
+  };
   return (
     <ul className="todo-list">
       {/* <li className="completed">
@@ -19,9 +23,16 @@ function Todolist() {
       {items.map((item) => (
         <li key={item.id} className={item.completed ? "completed" : ""}>
           <div className="view">
-            <input className="toggle" type="checkbox" onChange={() => dispatch(toggle({id: item.id}))} />
+            <input
+              className="toggle"
+              type="checkbox"
+              onChange={() => dispatch(toggle({ id: item.id }))}
+            />
             <label>{item.title}</label>
-            <button className="destroy"></button>
+            <button
+              className="destroy"
+              onClick={() => handleDestroy(item.id)}
+            ></button>
           </div>
         </li>
       ))}
--- src/redux/todos/todosSlice.js
+++ src/redux/todos/todosSlice.js
@@ -24,10 +24,15 @@ export const todosSlice = createSlice({
     toggle: (state, action) => {
       const { id } = action.payload;
       const item = state.items.find((item) => item.id === id);
-      item.completed = !item.completed
+      item.completed = !item.completed;
+    },
+    destroy: (state, action) => {
+      const id = action.payload;
+      const filtered = state.items.filter((item) => item.id !== id);
+      state.items = filtered;
     },
   },
 });
 
-export const { addTodo, toggle } = todosSlice.actions;
+export const { addTodo, toggle, destroy } = todosSlice.actions;
 export default todosSlice.reducer;
