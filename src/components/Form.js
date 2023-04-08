f58626de1a4f79986d235edb2f27dc1f85e5b6b6
myxsan <yksl3461@gmail.com>
2023-04-08 23:07:49 +0300

    adding todo element

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,26 @@
-import React from "react";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { nanoid } from "@reduxjs/toolkit";
+import { addTodo } from "../redux/todos/todosSlice";
 
 function Form() {
+  const dispatch = useDispatch();
+  const [title, setTitle] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addTodo({ id: nanoid(), title, completed: false }));
+    setTitle("");
+  };
   return (
-    <form mv-action="set(newTodo, newTodo.trim()), if(newTodo != '', add(newTodo, todo) & set(newTodo, ''))">
+    <form onSubmit={handleSubmit}>
       <input
         property="newTodo"
-        class="new-todo"
+        className="new-todo"
         placeholder="What needs to be done?"
-        autofocus
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        autoFocus
       />
     </form>
   );
---
+++ src/redux/todos/todosSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const todosSlice = createSlice({
+  name: "todos",
+  initialState: {
+    items: [
+      {
+        id: "1",
+        title: "Learn React",
+        completed: true,
+      },
+      {
+        id: "2",
+        title: "Read a book",
+        completed: false,
+      },
+    ],
+  },
+  reducers: {
+    addTodo: (state, action) => {
+        state.items.push(action.payload)
+    }
+  },
+});
+
+export const {addTodo} = todosSlice.actions
+export default todosSlice.reducer;
